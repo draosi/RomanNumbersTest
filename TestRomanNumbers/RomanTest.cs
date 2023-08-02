2b2703422bfa8f30dfc674005af6a41fd86bf7ca
draosi <draosi.dev@gmail.com>
2023-08-02 14:25:38 +0200

    test 2 au rouge

--- TestRomanNumbers/RomanTest.cs
+++ TestRomanNumbers/RomanTest.cs
@@ -5,13 +5,23 @@ namespace TestRomanNumbers
     public class RomanTest
     {
         [Fact(DisplayName = "ETANT DONNE le chiffre 1" +
-            "QUAND je le convertis en nombre romains" +
-            "ALORS j'obtiens 1 fois 1")]
+                            "QUAND je le convertis en nombre romains" +
+                            "ALORS j'obtiens 1 fois 1")]
         public void TestUn()
         {
             const uint chiffre = 1;
             var nombreRomain = ConvertisseurRoman.Transforme(chiffre);
             Assert.Equal("I", nombreRomain);
+        }
+
+        [Fact(DisplayName = "ETANT DONNE le chiffre 2" +
+                            "QUAND je le convertis en nombre romains" +
+                            "ALORS j'obtiens 1 fois 1")]
+        public void Testdeux()
+        {
+            const uint chiffre = 1;
+            var nombreRomain = ConvertisseurRoman.Transforme(chiffre);
+            Assert.Equal("II", nombreRomain);
         }
     }
 }
